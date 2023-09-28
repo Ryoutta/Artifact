b140fdc00f25f098c937fa8a9144d20923372638
Ryoutta <ryoutakuromaru@icloud.com>
2023-09-28 19:50:55 +0900

    seeder編集中

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'ryo',
-            'email' => 'ryoutakuromaru@icloud.com',
+            'email' => 'ryoutakuromaru@com',
             'password' => Hash::make('password'),
         ]);
          
